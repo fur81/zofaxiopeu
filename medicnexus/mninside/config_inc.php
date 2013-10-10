4695249cdc070bba364ba944b46b71e13955638b
Manuel Morejon <manuel.morejon.85@gmail.com>
2013-10-09 20:51:44 -0430

    se configura para enviar correos con smtp de gmail.

--- medicnexus/mninside/config_inc.php
+++ medicnexus/mninside/config_inc.php
@@ -9,18 +9,18 @@
         $g_default_language = 'spanish';
 	
 	# --- email --- #
-	$g_enable_email_notification = OFF;
-        #$g_phpMailer_method     = PHPMAILER_METHOD_SMTP;
-        #$g_smtp_host            = 'smtp.gmail.com';
-        #$g_smtp_username        = 'manuelminfo@gmail.com';
-        #$g_smtp_password        = 'casinodeportivo';
-        #$g_smtp_connection_mode = 'ssl';
-        #$g_smtp_port            = 465;
-        #$g_administrator_email  = 'manuel.morejon.85@gmail.com';
-        #$g_webmaster_email      = 'manuel.morejon.85@gmail.com';
-        #$g_from_name            = 'Medicnexus';
-        #$g_from_email           = 'manuel.morejon.85@gmail.com';
-        #$g_return_path_email    = 'manuel.morejon.85@gmail.com';
+		#$g_enable_email_notification = OFF;
+        $g_phpMailer_method     = PHPMAILER_METHOD_SMTP;
+        $g_smtp_host            = 'smtp.gmail.com';
+        $g_smtp_username        = 'manuelminfo@gmail.com';
+        $g_smtp_password        = 'casinodeportivo';
+        $g_smtp_connection_mode = 'ssl';
+        $g_smtp_port            = 465;
+        $g_administrator_email  = 'manuel.morejon.85@gmail.com';
+        $g_webmaster_email      = 'manuel.morejon.85@gmail.com';
+        $g_from_name            = 'Medicnexus';
+        $g_from_email           = 'manuel.morejon.85@gmail.com';
+        $g_return_path_email    = 'manuel.morejon.85@gmail.com';
 
         # --- branding --- #
         $g_window_title         = 'MNInside';
