cb3310bbfa3a426d46942d55fce3ca1e22b31cf2
Manuel Morejon <manuel.morejon.85@gmail.com>
2013-10-11 04:15:57 -0430

    se configura el email

--- medicnexus/mninside/config_inc.php
+++ medicnexus/mninside/config_inc.php
@@ -9,7 +9,7 @@
         $g_default_language = 'spanish';
 	
 	# --- email --- #
-	$g_enable_email_notification = OFF;
+		$g_enable_email_notification = OFF;
         #$g_phpMailer_method     = PHPMAILER_METHOD_SMTP;
         #$g_smtp_host            = 'smtp.gmail.com';
         #$g_smtp_username        = 'manuelminfo@gmail.com';
@@ -21,7 +21,11 @@
         #$g_from_name            = 'Medicnexus';
         #$g_from_email           = 'manuel.morejon.85@gmail.com';
         #$g_return_path_email    = 'manuel.morejon.85@gmail.com';
-
+		
+		#$g_validate_email
+		#se utiliza para validar los correos, tener en cuenta si incluirlos o no
+		
+		
         # --- branding --- #
         $g_window_title         = 'MNInside';
 ?>
