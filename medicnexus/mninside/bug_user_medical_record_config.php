1d70e1137f39b0fb87ea11763e69269bfbad8dd3
Manuel Morejon <manuel.morejon.85@gmail.com>
2013-10-13 03:35:25 -0430

    Se configura con los datos del servidor.

--- medicnexus/mninside/bug_user_medical_record_config.php
+++ medicnexus/mninside/bug_user_medical_record_config.php
@@ -1,6 +1,6 @@
 <?php
 // establish conecction with data base
-$proxyMySql = new mysqli ( 'localhost', 'root', 'carabobo', 'mninside' );
+$proxyMySql = new mysqli ( 'localhost', 'mninside', 'm4nt1s#2013', 'mninside' );
 if (mysqli_connect_errno ()) {
 	echo ("Failed to connect, the error message is : " . mysqli_connect_error ());
 	exit ();
