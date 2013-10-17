dc4a3c3c0056c1146cbb39963aff074fbb275935
Manuel Morejon <manuel.morejon.85@gmail.com>
2013-10-17 13:09:54 -0430

    Se agregan etiquetas asociadas a los proyectos.

--- medicnexus/mnintegration/src/utils/ca.php
+++ medicnexus/mnintegration/src/utils/ca.php
@@ -1,9 +1,15 @@
 <?php
 global $values;
 $values[L_CATALAN] = array(
-		/**  menu */
+		/**  headers */
 		'label_lastUpdate' => 'Darrera actualització',
 		'label_summary' => 'Resum',
-		'label_category' => 'Categoria'
+		'label_category' => 'Categoria',
+
+		/**  projects */
+		'label_project_second_opinion_title' => 'Segona Opinió',
+		'label_project_virtual_consultation_title' => 'Consulta Virtual',
+		'label_project_rapid_consultation_title' => 'Consulta Ràpida',
+		'label_project_health_program_title' => 'Programa de Salut'
 );
 ?>
--- medicnexus/mnintegration/src/utils/es.php
+++ medicnexus/mnintegration/src/utils/es.php
@@ -1,9 +1,15 @@
 <?php
 global $values;
 $values[L_SPANISH] = array(
-		/**  menu */
+		/**  headers  */
 		'label_lastUpdate' => 'Última Actualización',
 		'label_summary' => 'Resumen',
-		'label_category' => 'Categoría'
+		'label_category' => 'Categoría',
+		
+		/**  projects */
+		'label_project_second_opinion_title' => 'Segunda Opinión',
+		'label_project_virtual_consultation_title' => 'Consulta Virtual',
+		'label_project_rapid_consultation_title' => 'Consulta Rápida',
+		'label_project_health_program_title' => 'Programa de Salud'
 );
 ?>
