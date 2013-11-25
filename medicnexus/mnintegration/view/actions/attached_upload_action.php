e151429b7301700c22ce729f0638070b221b4bb0
Manuel Morejon <manuel.morejon.85@gmail.com>
2013-11-25 15:21:41 -0500

    Se eliminan los espacios del nombre.

--- medicnexus/mnintegration/view/actions/attached_upload_action.php
+++ medicnexus/mnintegration/view/actions/attached_upload_action.php
@@ -6,6 +6,9 @@ $fileType = $_FILES['fileAttached']['type'];
 $content = $_FILES['fileAttached']['tmp_name'];
 $content = file_get_contents($content);
 
+// se eliminan los espacios del nombre
+$name = str_replace(" ", "_", $name);
+
 // se carga el fichero al sistema
 $mantisCore->addAttachment($issueId, $name, $fileType, $content);
 ?>
