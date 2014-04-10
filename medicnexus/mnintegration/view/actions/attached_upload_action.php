a43071f38ef84b30a5e90374ef22dc85e2c69e24
Manuel Morejon <manuel.morejon.85@gmail.com>
2014-04-10 10:15:48 -0400

    Se chequea que no tenga errores.

--- medicnexus/mnintegration/view/actions/attached_upload_action.php
+++ medicnexus/mnintegration/view/actions/attached_upload_action.php
@@ -11,36 +11,41 @@
 
 /**
  * Acción para adjuntar un documento a la consulta ya existente.
- * 
+ *
  * @author Manuel Morejón
  * @copyright 2013 - 2014
  * @access public
- * 
+ *
  */
 
 // se obtienen los datos del formulario
 $issueId = $_POST['issueId'];
-if ( isset($_FILES['fileAttached']) ) {
+if ( isset($_FILES['fileAttached'])) {
 
-	$name = trim($_FILES['fileAttached']['name']);
-	$size = $_FILES['fileAttached']['size'];
-	$fileType = $_FILES['fileAttached']['type'];
-	$content = $_FILES['fileAttached']['tmp_name'];
-	$content = file_get_contents($content);
+	if ($_FILES['fileAttached']['error'] == 0) {
 
-	// se eliminan los espacios del nombre
-	$name = str_replace(" ", "_", $name);
+		$name = trim($_FILES['fileAttached']['name']);
+		$size = $_FILES['fileAttached']['size'];
+		$fileType = $_FILES['fileAttached']['type'];
+		$content = $_FILES['fileAttached']['tmp_name'];
+		$content = file_get_contents($content);
 
-	// se carga el fichero al sistema si no está vacía la dirección
-	if ( strlen($name) > 0 ) {
-		// la dirección tiene valor
-		if ($size < MN_MANTIS_FILE_MAX_SIZE ) {
-			// cumple con el tamanno requerido y se inserta el adjunto.
-			$mantisCore->addAttachment($issueId, $name, $fileType, $content);
-			$_SESSION ['msg'] = 'msg_info_upload_inserted';
-		} else {
-			$_SESSION ['msg'] = 'msg_error_upload_size';
+		// se eliminan los espacios del nombre
+		$name = str_replace(" ", "_", $name);
+
+		// se carga el fichero al sistema si no está vacía la dirección
+		if ( strlen($name) > 0 ) {
+			// la dirección tiene valor
+			if ($size < MN_MANTIS_FILE_MAX_SIZE ) {
+				// cumple con el tamanno requerido y se inserta el adjunto.
+				$mantisCore->addAttachment($issueId, $name, $fileType, $content);
+				$_SESSION ['msg'] = 'msg_info_upload_inserted';
+			} else {
+				$_SESSION ['msg'] = 'msg_error_upload_size';
+			}
 		}
+	} else {
+		$_SESSION ['msg'] = 'msg_error_upload_size';
 	}
 }
 ?>
