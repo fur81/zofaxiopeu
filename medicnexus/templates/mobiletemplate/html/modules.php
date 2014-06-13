b48ae813b6777210e0c555109336bf14a205e155
Manuel Morejon <manuel.morejon.85@gmail.com>
2014-06-13 11:59:16 -0400

    se agrega plugins mobiletemplate.

---
+++ medicnexus/templates/mobiletemplate/html/modules.php
@@ -0,0 +1,10 @@
+<?php
+/*------------------------------------------------------------------------
+
+-------------------------------------------------------------------------*/
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+/*
+ * Module chrome that allows for rounded corners by wrapping in nested div tags
+ */
